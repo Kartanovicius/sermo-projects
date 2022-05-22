31fe079bf5779980ff201c84dbd8e8407a8dd9ba
donataskartanovicius <donatas.karta@gmail.com>
2022-05-22 19:16:53 +0300

    Renamed

--- src/pages/HTTP404.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HTTP404() {
-  return (
-    <div>HTTP404</div>
-  )
-}
---
+++ src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProjectCard from '../components/ProjectCard'
+
+export default function Home() {
+  return (
+    <div>Main
+      <ProjectCard code="code" clientName='Novartis' projectName='abc'></ProjectCard>
+    </div>
+  )
+}
