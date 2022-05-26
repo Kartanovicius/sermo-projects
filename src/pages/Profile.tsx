f08e1c41acda88aec0ffef2c27a74d8bf292ac36
donataskartanovicius <donatas.karta@gmail.com>
2022-05-26 23:52:03 +0300

    Init commit

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { useParams } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Copyright from '../components/Copyright';
+
+
+function ProfileContent() {
+  let params = useParams();
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+                <div>{params.userid}</div>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Copyright sx={{ pt: 4 }} />
+    </Container>
+  );
+}
+
+export default function Profile() {
+  return <ProfileContent />;
+}
