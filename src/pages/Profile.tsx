6bc16b0b9fcc134b90bf15ff86a52b180d6a1904
donataskartanovicius <donatas.karta@gmail.com>
2022-05-29 21:13:16 +0300

    Remove unused imports

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
