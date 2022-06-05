dc4d8c3b4b3137c4645e9c78be98f6cf0c4efe08
donataskartanovicius <donatas.karta@gmail.com>
2022-06-05 11:18:28 +0300

    navigate to user profile

--- src/components/navbar/AvatarMenuItems.tsx
+++ src/components/navbar/AvatarMenuItems.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Logout } from '@mui/icons-material'
 import { MenuItem, Avatar, Divider, ListItemIcon } from '@mui/material'
 import { useAuth } from '../../context/authContext';
+import { useNavigate } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes'
 
 
 
 export const AvatarMenuItems = (firstName: any) => {
-  const { signOutUser } = useAuth();
+  const { signOutUser, currentUser } = useAuth();
   const firstNameVal = firstName.firstName
+  const navigate = useNavigate();
 
   return (
     <React.Fragment>
-    <MenuItem>
+    <MenuItem onClick={(e) => navigate(ROUTES.PROFILE+currentUser.uid)}>
       <Avatar /> <>{firstNameVal}</>
     </MenuItem>
     <Divider />
