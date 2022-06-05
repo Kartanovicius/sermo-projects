382597f36d14d940f5937224156df93cb7cef4d4
donataskartanovicius <donatas.karta@gmail.com>
2022-06-06 01:11:53 +0300

    Weather import

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 //mui
 import { Grid, Container, Typography } from '@mui/material';
 //components
-<<<<<<< HEAD
 import Projects from '../components/dashboard/Projects';
 import Weather from '../components/dashboard/Weather';
-=======
-import Weather from '../components/dashboard/Weather';
-import Projects from '../components/dashboard/Projects';
->>>>>>> 5a5089bdd0a1b7043322df54c39e80cd749f17af
 import useUser from '../hooks/use-user';
 import { useAuth } from '../context/authContext';
 
