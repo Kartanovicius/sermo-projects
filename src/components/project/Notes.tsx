dea5489b8c5f72d8820da178875eef829a9e62b5
donataskartanovicius <donatas.karta@gmail.com>
2022-07-23 17:17:43 +0300

    Notes is saved automatically

--- src/components/project/Notes.tsx
+++ src/components/project/Notes.tsx
@@ -1,5 +1,6 @@
-import { Skeleton, Card, Typography, TextField, Box, Button } from '@mui/material'
-import { useRef } from 'react'
+import { Skeleton, Card, Typography, TextField, Box, CircularProgress } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
+import useDebounce from '../../hooks/use-debounce'
 import { updateProjectByCode } from '../../services/firebase'
 import { IProject } from '../../types'
 
@@ -9,52 +10,54 @@ interface Props {
 
 export default function Notes ({project}: Props) {
   const textInputRef = useRef<HTMLInputElement>()
+  const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false);
+  const debounceSave = useDebounce(value, 2000)
 
-  function saveButtonHandler() {
-    if (project.code && textInputRef.current !== undefined) {
-      updateProjectByCode(project.code, {note: textInputRef.current.value})
+  useEffect(() => {
+    if (project.code) {
+      updateProjectByCode(project.code, {note: value})
+      .then(() => setLoading(false))
     }
-  }
-
+  }, [debounceSave])
+  
   return (
     <>
       {project.code === null ? <Skeleton height={350}/> :
         <Card sx={{
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
           p: {
             xs: 2,
             sm: 3,
           },
           gap: 1,
         }}>
-          <Typography variant='h6'>
-            Notes
-          </Typography>
+          <Box
+          sx={{
+            display: "inline-flex",
+            justifyContent: "space-between",
+          }}>
+            <Typography variant="h6">
+              Notes
+            </Typography>
+            {loading ? <CircularProgress size={24} sx={{ mr: 1 }}/> : <></>}
+          </Box>
           <TextField 
-          id='notes-input'
-          placeholder='Add any important note for this project'
+          id="notes-input"
+          placeholder="Add any important note for this project"
           fullWidth
           multiline
-          variant='filled'
-          sx={{ MuiFilledInput: { root: { p: '18px 18px 8px' } }}}
+          variant="filled"
+          sx={{ MuiFilledInput: { root: { p: "18px 18px 8px" } }}}
           maxRows={10}
           defaultValue={project.note}
+          onChange={(e) => {
+            setValue(e.currentTarget.value) 
+            setLoading(true)
+          }}
           inputRef={textInputRef}
           />
-          <Box sx={{
-            display: 'flex',
-            justifyContent: 'end',
-          }}>
-            <Button
-            id='save-notes-btn'
-            variant='contained'
-            sx={{ mt: 2 }}
-            onClick={() => saveButtonHandler()}
-            >
-              Save Note
-            </Button>
-          </Box>
         </Card>
       }
     </>
