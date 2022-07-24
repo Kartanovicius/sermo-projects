bb863621f1a06bbdabb5bbee0f259c516d6b6455
donataskartanovicius <donatas.karta@gmail.com>
2022-07-24 07:38:14 +0300

    Search input implemented

--- src/components/dashboard/Projects.tsx
+++ src/components/dashboard/Projects.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react'
 // Material-ui
-import { ListItemText, ListItemButton, List, ListSubheader, Divider, Button, Box } from '@mui/material'
-// Firebase
-import { DocumentData } from 'firebase/firestore'
-import { getUserProjects } from '../../services/firebase'
+import { ListItemText, ListItemButton, List, ListSubheader, Divider, Button, Box, TextField } from '@mui/material'
 // Contexts
 import { useAuth } from '../../context/authContext'
 // npm packages
@@ -13,12 +10,24 @@ import { useNavigate } from 'react-router-dom'
 import { CreateProjectDialog } from '../CreateProjectDialog'
 // Constants
 import * as ROUTES from '../../constants/routes'
+// Custom hooks
+import useDebounce from '../../hooks/use-debounce'
+// Redux toolkit
+import { fetchSearchedProjects, fetchUserProjects } from '../../store/features/project/projectsSlice'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
 
 function ProjectsContent() {
+  // Navigation
   const navigate = useNavigate()
   // Create project
   const createProjectDialog = useDialog(CreateProjectDialog)
   const [newProjectCreted, setNewProjectCreted] = useState(false)
+  // Search
+  const [searchValue, setSearchValue] = useState('')
+  const [searchActive, setSearchActive] = useState(false)
+  const searchDebounce = useDebounce(searchValue, 500)
+  // User
+  const { currentUser } = useAuth()
 
   async function CreateProjectDialogHandler() {
     await createProjectDialog.show('')
@@ -26,42 +35,61 @@ function ProjectsContent() {
   }
 
   // Display projects
-  const { currentUser } = useAuth()
-  const [projectList, setProjectList] = useState<DocumentData[]>([])
+  // Dispatch redux functions for fetching projects
+  const { projects } = useAppSelector(state => state.projects)
+  const dispatch = useAppDispatch()
 
+  // Search project only every 500ms and when there is > 2 chars enetered
   useEffect(() => {
-    getUserProjects(currentUser.uid).then(res => {
-      let projects: DocumentData[] = []
-      if (res !== undefined) {
-        res.forEach(res => {
-          return projects.push(res)
-        })
-      }
-      return setProjectList(projects)
-    })
+    if (searchValue.length > 2) {
+      dispatch(fetchSearchedProjects(searchValue))
+      setSearchActive(true)
+    }
+    // Do not fetch data if search haven't been activated
+    else if (searchActive) {
+      dispatch(fetchUserProjects(currentUser.uid))
+      setSearchActive(false)
+    }
+  },[searchDebounce])
+
+  // Fetch data initaily and when new project created
+  useEffect(() => {
+    dispatch(fetchUserProjects(currentUser.uid))
     setNewProjectCreted(false)
-  }, [currentUser.uid, newProjectCreted])
+  }, [newProjectCreted, currentUser.uid])
+  
 
   return (
     <>
       <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
+        display: "flex",
+        justifyContent: "space-between",
         mb: 2,
+        flexDirection: {
+          xs: "column",
+          sm: "row",
+        },
+        gap: 1,
       }}>
-        <Button variant='contained' onClick={(e) => CreateProjectDialogHandler()}>Create new project</Button>
+        <Button variant="contained" onClick={() => CreateProjectDialogHandler()}>Create new project</Button>
+        <TextField 
+          variant="filled"
+          placeholder="Project Search"
+          sx={{ ".MuiFilledInput-root": { height: 39 }, "input": { py: 0 } }}
+          onChange={(e) => setSearchValue(e.currentTarget.value)}
+        />
       </Box>
       <List
-      sx={{bgcolor: 'background.paper', borderRadius: 1, padding: 0}}
+      sx={{bgcolor: "background.paper", borderRadius: 1, padding: 0}}
       subheader={
-        <ListSubheader component='div' id='list-subheader' sx={{borderRadius: 1}}>
+        <ListSubheader component="div" id="list-subheader" sx={{borderRadius: 1}}>
           Projects
         </ListSubheader>
       }>
         <Divider/>
-          {[...projectList].map(project => (
-            <ListItemButton component='div' key={project.code} onClick={(e: any) => navigate(ROUTES.PROJECT + project.code)}>
+          {projects?.map(project => (
+            <ListItemButton component="div" key={project.code} onClick={() => navigate(ROUTES.PROJECT + project.code)}>
               <ListItemText primary={`${project.code} - ${project.client} - ${project.name}`}/>
             </ListItemButton>
           ))}
