99fcbe905607f5d192571f95130702bc32dbad56
donataskartanovicius <donatas.karta@gmail.com>
2022-07-24 17:36:53 +0300

    Firebase keys changed to .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqmqyU1t21_0002MlSOR8yKTXS1uxr2A8",
-  authDomain: "sermo-projects.firebaseapp.com",
-  projectId: "sermo-projects",
-  storageBucket: "sermo-projects.appspot.com",
-  messagingSenderId: "652494557455",
-  appId: "1:652494557455:web:5b09528f0bfeb9adfa2e7a",
-  measurementId: "G-MF9KNT4H1E"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const firebase = initializeApp(firebaseConfig);
