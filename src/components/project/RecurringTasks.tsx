924224a590741b0669694356ce595fb4be42074e
donataskartanovicius <donatas.karta@gmail.com>
2022-07-25 19:13:45 +0300

    Shows error if empty fields

--- src/components/project/RecurringTasks.tsx
+++ src/components/project/RecurringTasks.tsx
@@ -1,6 +1,15 @@
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 // Material-UI
-import { Skeleton, Card, Typography, TextField, Box, Button, IconButton, Tooltip } from '@mui/material'
+import {
+  Skeleton,
+  Card,
+  Typography,
+  TextField,
+  Box,
+  Button,
+  IconButton,
+  Tooltip,
+} from '@mui/material'
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 // Firebase
 import { updateProjectByCode } from '../../services/firebase'
@@ -10,73 +19,75 @@ import moment from 'moment'
 import { IProject, IRecurringTask } from '../../store/features/project/project.types'
 // Components
 import RecurringTasksList from './RecurringTasksList'
+// Constants
+import * as ALERTS from '../../constants/alerts'
 
 interface Props {
   project: IProject
 }
 
-export default function RecurringTasks ({ project }: Props) {
+export default function RecurringTasks({ project }: Props) {
   const { code, recurringTasks } = project
 
+  const [taskTextError, setTaskTextError] = useState(false)
+  const [taskTimeError, setTaskTimeError] = useState(false)
   const taskTextRef = useRef<HTMLInputElement>()
   const taskTimeRef = useRef<HTMLInputElement>()
-  const [recurringTasksList, setRecurringTasksList] = useState<IProject['recurringTasks']>(recurringTasks)
+  const [recurringTasksList, setRecurringTasksList] =
+    useState<IProject['recurringTasks']>(recurringTasks)
 
   const [openList, setOpenList] = useState(false)
 
   const createRecurringTaskButtonHandler = async () => {
-    if (
-      code && 
-      taskTextRef.current !== undefined && 
-      taskTimeRef.current !== undefined &&
-      taskTextRef.current.value !== '' &&
-      taskTimeRef.current.value !== ''
-    ) {
-      // Convert input time to milliseconds 
-      let hour = parseInt(taskTimeRef.current.value.slice(0, 2))
-      let minute = parseInt(taskTimeRef.current.value.slice(-2))
-      let time = moment().set('h', hour).set('m', minute).add(1, 'd').valueOf()
-      // Task object
-      const recurringTask = {
-        task: taskTextRef.current.value,
-        time: time,
-        done: true
-      }
-      // Push new task to recurringTasks if it exist
-      // If recurringTasks does not exist then create a new array with recurringTask
-      let newRecurringTasks = [...recurringTasksList]
-      newRecurringTasks !== undefined ? 
-      newRecurringTasks.push(recurringTask) :
-      newRecurringTasks = [recurringTask]
-      // Push new task to Firestore
-      try {  
-        await updateProjectByCode(
-          code, 
-          { recurringTasks: newRecurringTasks }
-        )
-        // Clear input fields
-        taskTextRef.current.value = ''
-        taskTimeRef.current.value = ''
+    if (code && taskTextRef.current !== undefined && taskTimeRef.current !== undefined) {
+      if (taskTextRef.current.value !== '' && taskTimeRef.current.value !== '') {
+        // Convert input time to milliseconds
+        const hour = parseInt(taskTimeRef.current.value.slice(0, 2))
+        const minute = parseInt(taskTimeRef.current.value.slice(-2))
+        const time = moment().set('h', hour).set('m', minute).add(1, 'd').valueOf()
+        // Task object
+        const recurringTask = {
+          task: taskTextRef.current.value,
+          time: time,
+          done: true,
+        }
+        // Push new task to recurringTasks if it exist
+        // If recurringTasks does not exist then create a new array with recurringTask
+        let newRecurringTasks = [...recurringTasksList]
+        newRecurringTasks !== undefined
+          ? newRecurringTasks.push(recurringTask)
+          : (newRecurringTasks = [recurringTask])
+        // Push new task to Firestore
+        try {
+          await updateProjectByCode(code, { recurringTasks: newRecurringTasks })
+          // Clear input fields
+          taskTextRef.current.value = ''
+          taskTimeRef.current.value = ''
 
-        setRecurringTasksList(newRecurringTasks)
-      } catch (error) {
-        console.log(error)
+          setRecurringTasksList(newRecurringTasks)
+        } catch (error) {
+          console.log(error)
+        }
       }
+      taskTimeRef.current.value === '' && setTaskTimeError(true)
+      taskTextRef.current.value === '' && setTaskTextError(true)
     }
   }
 
   const deleteHandler = (code: number, index: number) => {
-    let newRecurringTasks = [...recurringTasksList]
+    const newRecurringTasks = [...recurringTasksList]
     newRecurringTasks.splice(index, 1)
-    updateProjectByCode(code, { recurringTasks: newRecurringTasks })
-    .then(() => setRecurringTasksList(newRecurringTasks))
+    updateProjectByCode(code, { recurringTasks: newRecurringTasks }).then(() =>
+      setRecurringTasksList(newRecurringTasks),
+    )
   }
 
   const doneCheckboxHandler = (code: number, item: IRecurringTask, index: number) => {
-    let newRecurringTasks = [...recurringTasksList]
+    const newRecurringTasks = [...recurringTasksList]
     newRecurringTasks[index]['done'] = !item.done
-    updateProjectByCode(code, { recurringTasks: newRecurringTasks })
-    .then(() => setRecurringTasksList(newRecurringTasks))
+    updateProjectByCode(code, { recurringTasks: newRecurringTasks }).then(() =>
+      setRecurringTasksList(newRecurringTasks),
+    )
   }
 
   // Open table if recurringTasks exist and if it has any items
@@ -94,15 +105,16 @@ export default function RecurringTasks ({ project }: Props) {
 
   useEffect(() => {
     return () => {
-      let newRecurringTasks = [...recurringTasksList]
+      const newRecurringTasks = [...recurringTasksList]
       recurringTasksList.forEach((task, index) => {
         if (task.time - moment().valueOf() < 0 && code !== undefined && code !== null) {
           newRecurringTasks[index]['done'] = false
           while (task.time - moment().valueOf() < 0) {
             newRecurringTasks[index]['time'] = moment(task.time).add(1, 'd').valueOf()
           }
-          updateProjectByCode(code, { recurringTasks: newRecurringTasks })
-          .then(() => setRecurringTasksList(newRecurringTasks))
+          updateProjectByCode(code, { recurringTasks: newRecurringTasks }).then(() =>
+            setRecurringTasksList(newRecurringTasks),
+          )
         }
       })
     }
@@ -110,61 +122,73 @@ export default function RecurringTasks ({ project }: Props) {
 
   return (
     <>
-      {code === null ? <Skeleton height={350}/> :
-        <Card sx={{
-          display: "flex",
-          flexDirection: "column",
-          p: {
-            xs: 2,
-            sm: 3,
-          },
-          gap: 1,
-        }}>
-          <Box sx={{
-            display: "inline-flex",
-            justifyContent: "space-between",
-          }}>
-            <Typography variant="h6">
-              Recurring Tasks
-            </Typography>
-            <Tooltip disableFocusListener title="Checkbox will automatically deselect every 24h on selected time">
+      {code === null ? (
+        <Skeleton height={350} />
+      ) : (
+        <Card
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            p: {
+              xs: 2,
+              sm: 3,
+            },
+            gap: 1,
+          }}
+        >
+          <Box
+            sx={{
+              display: 'inline-flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant='h6'>Recurring Tasks</Typography>
+            <Tooltip
+              disableFocusListener
+              title='Checkbox will automatically deselect every 24h on selected time'
+            >
               <IconButton>
                 <InfoOutlinedIcon />
               </IconButton>
             </Tooltip>
           </Box>
-          <Box sx={{
-            display: "inline-flex",
-            flexDirection: {
-              xs: "column",
-              sm: "row",
-            },
-            gap: 2,
-            ".MuiTextField-root": {
-              "&:nth-of-type(1)": {
-                width: {
-                  xs: "100%",
-                  sm: "70%",
-                }
+          <Box
+            sx={{
+              display: 'inline-flex',
+              flexDirection: {
+                xs: 'column',
+                sm: 'row',
               },
-              width: {
-                xs: "100%",
-                sm: "30%",
-              }
-            }
-          }}>
+              gap: 2,
+              '.MuiTextField-root': {
+                '&:nth-of-type(1)': {
+                  width: {
+                    xs: '100%',
+                    sm: '70%',
+                  },
+                },
+                width: {
+                  xs: '100%',
+                  sm: '30%',
+                },
+              },
+            }}
+          >
             <TextField
-              id="task-text-input"
-              label="Task"
-              variant="filled"
+              id='task-text-input'
+              label='Task'
+              variant='filled'
               inputRef={taskTextRef}
+              error={taskTextError}
+              helperText={taskTextError ? ALERTS.EMPTY_FIELD : ''}
+              onChange={() => setTaskTextError(false)}
             />
-            <TextField 
-              id="task-time-input"
-              label="Repeat at"
-              variant="filled"
-              type="time"
-              defaultValue="07:30"
+            <TextField
+              id='task-time-input'
+              label='Repeat at'
+              variant='filled'
+              type='time'
+              defaultValue='07:30'
               InputLabelProps={{
                 shrink: true,
               }}
@@ -173,41 +197,46 @@ export default function RecurringTasks ({ project }: Props) {
               }}
               sx={{ width: 150 }}
               inputRef={taskTimeRef}
+              error={taskTimeError}
+              helperText={taskTimeError ? ALERTS.EMPTY_FIELD : ''}
+              onChange={() => setTaskTimeError(false)}
             />
           </Box>
-          <Box sx={{
-            display: "flex",
-            justifyContent: "end",
-          }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'end',
+            }}
+          >
             <Button
-            id="display-list-btn"
-            variant="text"
-            sx={{ mt: 2, mr: 1 }}
-            disabled={recurringTasksList.length === 0}
-            onClick={() => openListHandler()}
+              id='display-list-btn'
+              variant='text'
+              sx={{ mt: 2, mr: 1 }}
+              disabled={recurringTasksList.length === 0}
+              onClick={() => openListHandler()}
             >
               Show Tasks
             </Button>
             <Button
-            id="add-item-to-list-btn"
-            variant="contained"
-            sx={{ mt: 2 }}
-            onClick={() => createRecurringTaskButtonHandler()}
+              id='add-item-to-list-btn'
+              variant='contained'
+              sx={{ mt: 2 }}
+              onClick={() => createRecurringTaskButtonHandler()}
             >
               Create Task
             </Button>
           </Box>
           <Box>
-            <RecurringTasksList 
-              recurringTasksList={recurringTasksList} 
-              code={code} 
-              open={openList} 
+            <RecurringTasksList
+              recurringTasksList={recurringTasksList}
+              code={code}
+              open={openList}
               deleteHandler={deleteHandler}
               doneCheckboxHandler={doneCheckboxHandler}
             />
           </Box>
         </Card>
-      }
+      )}
     </>
   )
 }
--- src/constants/alerts.ts
+++ src/constants/alerts.ts
@@ -6,5 +6,6 @@ export const PASSWORD_UPDATED = 'Password updated successfuly!'
 
 // Error
 export const INVALID_PASSWORD = 'You have entered an invalid password'
-export const UNFINISHED_DIALOG = 'You didn\'t finished filling form. Changes will not be done!'
+export const UNFINISHED_DIALOG = "You didn't finished filling form. Changes will not be done!"
 export const PROJECT_CODE_ALREADY_EXISTS = 'This project code already exist in database!'
+export const EMPTY_FIELD = 'Field is empty'
