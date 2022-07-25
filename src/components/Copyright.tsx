7daa9fee00b9a4d4397a2de907c7af794861a233
donataskartanovicius <donatas.karta@gmail.com>
2022-07-25 19:12:54 +0300

    Updated text

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
 
 export default function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    <Typography variant='body2' color='text.secondary' align='center' {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Survey Project Manager
+      <Link color='inherit' href='https://github.com/Kartanovicius'>
+        Yogurt/Kartanovicius
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
